17c3774562fb3b40a5f5bde51497cee22e250096
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2018-07-22 16:53:16 +0800

    Log: if(!(work_mode & MODE_BATCH)) { xxx }

--- include/debug.h
+++ include/debug.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include "monitor.h"
+
 extern void print_registers();
 
 #define eprintf(...) fprintf(stderr, ## __VA_ARGS__)
@@ -13,15 +15,17 @@ extern void print_registers();
 // we are properly doing diff testing in batch_mode, so do not Log in batch_mode
 #define Log(format, ...) \
   do { \
+	if(!(work_mode & MODE_BATCH)) { \
       printf("\e[1;34m[%s,%d,%s] " format "\e[0m\n", \
           __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
+	} \
   } while (0)
 
 #define Assert(cond, fmt, ...) do { \
     if (!(cond)) { \
 	  eprintf("nemu: %s:%d: %s: Assertion `%s' failed\n", \
 			  __FILE__, __LINE__, __func__, #cond); \
-      eprintf("\33[1;31m" fmt "\33[0m\n", ## __VA_ARGS__); \
+      eprintf("\e[1;31m" fmt "\e[0m\n", ## __VA_ARGS__); \
 	  abort(); \
     } \
   } while (0)
@@ -33,7 +37,7 @@ extern void print_registers();
 	  eprintf("=========== dump registers =========\n"); \
 	  print_registers(); \
 	  eprintf("=========== dump    end =========\n"); \
-      eprintf("\33[1;31m" fmt "\33[0m\n", ## __VA_ARGS__); \
+      eprintf("\e[1;31m" fmt "\e[0m\n", ## __VA_ARGS__); \
 	  abort(); \
     } \
   } while (0)
