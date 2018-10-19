a20cf65b49280f90a6ec252d95216b76d80f2519
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2018-10-19 10:17:41 +0800

    NR_IQ -> NR_INSTR

--- src/cpu/iq.c
+++ src/cpu/iq.c
@@ -11,13 +11,13 @@ typedef struct {
 static int pc_ptr = 0, instr_ptr = 0;
 static iq_element_t iq[8];
 
-#define NR_IQ (sizeof(iq) / sizeof(iq[0]))
+#define NR_INSTR (sizeof(iq) / sizeof(iq[0]))
 
 void instr_enqueue_pc(vaddr_t pc) {
   instr_ptr = pc_ptr;
   iq[pc_ptr].pc = pc;
   iq[pc_ptr].instr_enq = false;
-  pc_ptr = (pc_ptr + 1) % NR_IQ;
+  pc_ptr = (pc_ptr + 1) % NR_INSTR;
 }
 
 void instr_enqueue_instr(uint32_t instr) {
@@ -32,13 +32,13 @@ uint32_t get_current_instr() {
 }
 
 void print_instr_queue(void) {
-  eprintf("last executed %ld instrs:\n", NR_IQ);
+  eprintf("last executed %ld instrs:\n", NR_INSTR);
   int i = pc_ptr;
   do {
 	if(iq[i].instr_enq)
 	  eprintf("0x%08x: %08x\n", iq[i].pc, iq[i].instr);
 	else
 	  eprintf("0x%08x: xxxxxxxx\n", iq[i].pc);
-	i = (i + 1) % NR_IQ;
+	i = (i + 1) % NR_INSTR;
   } while(i != pc_ptr);
 }
