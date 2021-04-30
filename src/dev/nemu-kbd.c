178c4f00335f8ca8909e58b0a56deafcf8e7ed9c
ouxianfei <ouxianfei@smail.nju.edu.cn>
2021-04-30 22:01:11 +0800

    use fifo to refactor nemu-keyboard, fix call to SDL_PeepEvents, should invoke SDLPumpEvents firstly

--- src/dev/nemu-kbd.c
+++ src/dev/nemu-kbd.c
@@ -4,20 +4,16 @@
 #include "device.h"
 #include "utils/file.h"
 #include "utils/sdlkey.h"
+#include "utils/fifo.h"
 
 #define NEMU_KEYBOARD_CODE 0x0
 #define NEMU_KEYBOARD_STAT 0x4
 #define NEMU_KEYBOARD_SIZE 0x10
 
-/////////////////////////////////////////////////////////////////
-//                     kb simulation //
-/////////////////////////////////////////////////////////////////
-
-#define KEYBOARD_QUEUE_LEN 1024
-static int nemu_keyboard_queue[KEYBOARD_QUEUE_LEN];
-static int nemu_keyboard_f = 0, nemu_keyboard_r = 0;
+static fifo_type(uint32_t, 1024) nemu_kbd_queue;
 
 static bool nemu_keyboard_update_irq() {
+  SDL_PumpEvents();
   SDL_Event evt = {0};
   int cnt = SDL_PeepEvents(&evt, 1, SDL_GETEVENT,
       SDL_EVENTMASK(SDL_KEYDOWN) |
@@ -26,27 +22,19 @@ static bool nemu_keyboard_update_irq() {
 
   uint32_t scancode =
       SDLKey_to_scancode(evt.type, evt.key.keysym.sym);
-  int next = (nemu_keyboard_r + 1) % KEYBOARD_QUEUE_LEN;
-  if (next != nemu_keyboard_f) { // if not full
-    nemu_keyboard_queue[nemu_keyboard_r] = scancode;
-    nemu_keyboard_r = next;
-    /* no irq */
-  }
+  fifo_push(nemu_kbd_queue, scancode);
   return false;
 }
 
 static uint32_t nemu_keyboard_read(paddr_t addr, int len) {
   check_ioaddr(addr, len, NEMU_KEYBOARD_SIZE, "kb.read");
+  nemu_keyboard_update_irq();
   /* CTRL not yet implemented, only allow byte read/write */
   switch (addr) {
   case NEMU_KEYBOARD_STAT:
-    return nemu_keyboard_f == nemu_keyboard_r ? 0 : 1;
-  case NEMU_KEYBOARD_CODE: {
-    uint32_t code = nemu_keyboard_queue[nemu_keyboard_f];
-    nemu_keyboard_f =
-        (nemu_keyboard_f + 1) % KEYBOARD_QUEUE_LEN;
-    return code;
-  }
+    return !fifo_is_empty(nemu_kbd_queue);
+  case NEMU_KEYBOARD_CODE:
+    return fifo_pop(nemu_kbd_queue);
   default:
     CPUAssert(
         false, "kb: address(0x%08x) is not readable", addr);
