f8e308dc501c57dda9afbb4eb30735b6ee8f5268
ouxianfei <ouxianfei@smail.nju.edu.cn>
2021-04-30 22:35:37 +0800

    refactor nemu-kbd, compress kbd_peek and kbd_read

--- src/dev/nemu-kbd.c
+++ src/dev/nemu-kbd.c
@@ -2,9 +2,9 @@
 #include <stdbool.h>
 
 #include "device.h"
+#include "utils/fifo.h"
 #include "utils/file.h"
 #include "utils/sdlkey.h"
-#include "utils/fifo.h"
 
 #define NEMU_KEYBOARD_CODE 0x0
 #define NEMU_KEYBOARD_STAT 0x4
@@ -26,7 +26,8 @@ static bool nemu_keyboard_update_irq() {
   return false;
 }
 
-static uint32_t nemu_keyboard_peek(paddr_t addr, int len) {
+static ALWAYS_INLINE uint32_t nemu_keyboard_rp(
+    paddr_t addr, int len, bool poll_data) {
   check_ioaddr(addr, len, NEMU_KEYBOARD_SIZE, "kb.read");
   nemu_keyboard_update_irq();
   /* CTRL not yet implemented, only allow byte read/write */
@@ -34,7 +35,10 @@ static uint32_t nemu_keyboard_peek(paddr_t addr, int len) {
   case NEMU_KEYBOARD_STAT:
     return !fifo_is_empty(nemu_kbd_queue);
   case NEMU_KEYBOARD_CODE:
-    return fifo_top(nemu_kbd_queue);
+    if (poll_data)
+      return fifo_pop(nemu_kbd_queue);
+    else
+      return fifo_top(nemu_kbd_queue);
   default:
     CPUAssert(
         false, "kb: address(0x%08x) is not readable", addr);
@@ -43,21 +47,12 @@ static uint32_t nemu_keyboard_peek(paddr_t addr, int len) {
   return 0;
 }
 
+static uint32_t nemu_keyboard_peek(paddr_t addr, int len) {
+  return nemu_keyboard_rp(addr, len, false);
+}
+
 static uint32_t nemu_keyboard_read(paddr_t addr, int len) {
-  check_ioaddr(addr, len, NEMU_KEYBOARD_SIZE, "kb.read");
-  nemu_keyboard_update_irq();
-  /* CTRL not yet implemented, only allow byte read/write */
-  switch (addr) {
-  case NEMU_KEYBOARD_STAT:
-    return !fifo_is_empty(nemu_kbd_queue);
-  case NEMU_KEYBOARD_CODE:
-    return fifo_pop(nemu_kbd_queue);
-  default:
-    CPUAssert(
-        false, "kb: address(0x%08x) is not readable", addr);
-    break;
-  }
-  return 0;
+  return nemu_keyboard_rp(addr, len, true);
 }
 
 DEF_DEV(nemu_keyboard_dev) = {
