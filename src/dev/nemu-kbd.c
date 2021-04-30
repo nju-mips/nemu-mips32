dacdbadb9ab5b5bcd5fb021251b996b13998bb31
ouxianfei <ouxianfei@smail.nju.edu.cn>
2021-04-30 22:30:26 +0800

    add nemu_keyboard_peek

--- src/dev/nemu-kbd.c
+++ src/dev/nemu-kbd.c
@@ -26,6 +26,23 @@ static bool nemu_keyboard_update_irq() {
   return false;
 }
 
+static uint32_t nemu_keyboard_peek(paddr_t addr, int len) {
+  check_ioaddr(addr, len, NEMU_KEYBOARD_SIZE, "kb.read");
+  nemu_keyboard_update_irq();
+  /* CTRL not yet implemented, only allow byte read/write */
+  switch (addr) {
+  case NEMU_KEYBOARD_STAT:
+    return !fifo_is_empty(nemu_kbd_queue);
+  case NEMU_KEYBOARD_CODE:
+    return fifo_top(nemu_kbd_queue);
+  default:
+    CPUAssert(
+        false, "kb: address(0x%08x) is not readable", addr);
+    break;
+  }
+  return 0;
+}
+
 static uint32_t nemu_keyboard_read(paddr_t addr, int len) {
   check_ioaddr(addr, len, NEMU_KEYBOARD_SIZE, "kb.read");
   nemu_keyboard_update_irq();
@@ -48,5 +65,6 @@ DEF_DEV(nemu_keyboard_dev) = {
     .start = CONFIG_NEMU_KEYBOARD_BASE,
     .size = NEMU_KEYBOARD_SIZE,
     .read = nemu_keyboard_read,
+    .peek = nemu_keyboard_peek,
     .update_irq = nemu_keyboard_update_irq,
 };
